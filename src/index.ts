0f91a110168ee767c1ad233c2616968c75d20104
Eric Bouck <eric@lightyear.dev>
2025-07-28 11:49:07 -0700

    More examples

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,29 @@ import {
   defineCollection,
   defineCustomApp,
   defineIntegration,
+  defineModel,
+  defineOAuthConnector,
 } from "@runlightyear/sdk";
 
-defineCollection("hello-world").deploy();
+defineCollection("task-management")
+  .withTitle("Task Management")
+  .withModel(
+    defineModel("task")
+      .withTitle("Task")
+      .withSchema({
+        type: "object",
+        properties: {
+          id: { type: "string" },
+          title: { type: "string" },
+          description: { type: "string" },
+          completed: { type: "boolean" },
+        },
+        required: ["id", "title", "description", "completed"],
+        additionalProperties: false,
+      })
+      .deploy()
+  )
+  .deploy();
 
 const todoCustomApp = defineCustomApp("todo-app", "APIKEY")
   .withTitle("Todo App")
@@ -31,3 +51,25 @@ defineIntegration("todo-app")
       .deploy(),
   ])
   .deploy();
+
+const hubspotOAuth = defineOAuthConnector("hubspot")
+  .withAuthUrl("https://app.hubspot.com/oauth/authorize")
+  .withTokenUrl("https://api.hubapi.com/oauth/v1/token")
+  .withScope([
+    "crm.objects.contacts.read",
+    "crm.objects.contacts.write",
+    "crm.objects.companies.write",
+    "crm.objects.companies.read",
+    "crm.objects.deals.read",
+    "crm.objects.deals.write",
+    "crm.objects.users.read",
+    "crm.objects.owners.read",
+  ])
+  .build();
+
+defineCustomApp("hubspot", "OAUTH2")
+  .withTitle("Hubspot")
+  .withOAuthConnector(hubspotOAuth)
+  .deploy();
+
+defineIntegration("hubspot").withTitle("Hubspot").withApp("hubspot").deploy();
